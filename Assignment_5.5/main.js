66199c5c2effe5b6e564ce527f07b0bf70c7a4ba
Marius Krenevičius <86297482+m4rikas@users.noreply.github.com>
2022-02-02 14:38:20 +0200

    Fixed formatting and inconsistencies

--- Assignment_5.5/main.js
+++ Assignment_5.5/main.js
@@ -1,6 +1,6 @@
+const API_KEY = "";
 const request = url => {
     return new Promise((resolve, reject) => {
-
         fetch(url).then(response => {
             if(response.ok) {
                 resolve(response.json());
@@ -10,20 +10,18 @@ const request = url => {
                 });
             }
         });
-
     });
 };
 
 const WeatherInfo = async ( element, weather ) => {
     try {
         let city = weather.querySelector('#city').value;
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_KEY`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
         let response = await request(url);
         element.innerText = JSON.stringify(response);
-
-    } catch(err) {
+      } catch(err) {
         console.log(err);
-    }
+      }
 };
 
 
